38146d6a0cac532943b5f95c9621e2975cfdca99
sbtalk71 <sbtalk@yahoo.com>
2022-09-02 17:55:21 +0530

    updated

--- demo_misc/src/demo_misc/Main.java
+++ demo_misc/src/demo_misc/Main.java
@@ -11,12 +11,12 @@ public class Main {
 		GenericsApp<Integer> app1=new GenericsApp<>();
 		app1.setData(3, 4);
 		
-		System.out.println(app1.getData());
+		//System.out.println(app1.getData());
 		
 		GenericsApp<String> app2=new GenericsApp<>();
 		app2.setData("John", "Doe");
 		
-		System.out.println(app2.getData());
+		//System.out.println(app2.getData());
 
 	}
 
